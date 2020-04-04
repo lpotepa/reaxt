db39db4937da21289b1339e37249cbfb12152ff6
Łukasz Potępa <lpotepa@o2.pl>
2020-04-04 09:43:40 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Reaxt.Mixfile do
   defp deps do
     [{:exos, "1.0.0"},
      {:poolboy, "~> 1.5.0"},
-     {:cowboy,"~> 1.0.0"},
+     {:cowboy,"~> 2.5"},
      {:plug, "~> 1.0"},
      {:poison,"~> 2.1.0"},
      {:ex_doc, ">= 0.0.0", only: :dev}]
